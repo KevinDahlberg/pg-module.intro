faf7f63b5ab5497302e129f6e95c8cb07a6b838f
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-22 10:20:34 -0500

    database logging in console and bugs connecting to server fixed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,11 @@
 $(function(){
   console.log("JQ F-in Sourced!");
+
+  $.ajax({
+    type: "GET",
+    url: "/books",
+    success: function(response){
+      console.log("in response path", response);
+    }
+  });
 });
