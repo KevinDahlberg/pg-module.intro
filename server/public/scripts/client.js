84f7cce930d0ef064d82e74a7a16cf3fbb464a31
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-22 17:42:31 -0500

    server side working, trying to figure out buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,50 +1,16 @@
+//Global Variables
+var bookId;
+
 $(function(){
   console.log("JQ F-in Sourced!");
   getBooks();
-
-  $('#bookForm').on('submit', function(event){
-    event.preventDefault();
-    console.log($('#title').val(), $('#author').val());
-    $.ajax({
-      type: "POST",
-      url: "/books/add",
-      data: {title: $('#title').val(), author:$('#author').val(), publisher: $('#publisher').val(), year: $('#year').val()},
-      success: function(response) {
-        getBooks();
-        submitFormClear();
-      }
-    });
-  });// end bookForm click event
-
-  $('#searchForm').on('submit', function (event){
-    event.preventDefault();
-    console.log("in search form path");
-    var searchTerm = $('#search').val();
-    $.ajax({
-      type: "GET",
-      url: "books/search/" + searchTerm + "/",
-      success: function(response){
-        submitFormClear();
-        $('#/books').empty();
-        var book = response;
-        $("#books").append('<tr>');
-        $el = $('#books').children().last();
-        $el.append('<td>' + book.id + '</td>');
-        $el.append('<td>' + book.author + '</td>');
-        $el.append('<td>' + book.title + '</td>');
-        $el.append('<td>' + book.publisher + '</td>');
-        $el.append('<td>' + book.year + '</td>');
-        $el.append("<td><button>Delete</button></td>");
-      }
-    });
-  });//end click function
 
   function getBooks(){
     $.ajax({
       type: "GET",
       url: "/books",
       success: function(response){
-        console.log("in response path", response);
+        console.log("in getBooks response path");
         $('#books').empty();
         for (var i = 0; i < response.length; i++) {
           var book = response[i];
@@ -72,6 +38,47 @@ $(function(){
     }); // end AJAX
   } //end getBooks
 
+  //POST function to add new book
+  $('#bookForm').on('submit', function(event){
+    event.preventDefault();
+    console.log($('#title').val(), $('#author').val());
+    $.ajax({
+      type: "POST",
+      url: "/books/add",
+      data: {title: $('#title').val(), author:$('#author').val(), publisher: $('#publisher').val(), year: $('#year').val()},
+      success: function(response) {
+        console.log("book added");
+        getBooks();
+        submitFormClear();
+      }
+    });
+  });// end bookForm click event
+
+  //Search form
+  $('#searchForm').on('submit', function (event){
+    event.preventDefault();
+    console.log("in search form path");
+    var searchTerm = $('#search').val();
+    $.ajax({
+      type: "GET",
+      url: "books/search/" + searchTerm + "/",
+      success: function(response){
+        submitFormClear();
+        $('#/books').empty();
+        var book = response;
+        $("#books").append('<tr>');
+        $el = $('#books').children().last();
+        $el.append('<td>' + book.id + '</td>');
+        $el.append('<td>' + book.author + '</td>');
+        $el.append('<td>' + book.title + '</td>');
+        $el.append('<td>' + book.publisher + '</td>');
+        $el.append('<td>' + book.year + '</td>');
+        $el.append("<td><button>Delete</button></td>");
+      }
+    });
+  });//end click function
+
+  //Delet function
   $('#books').on('click', '.delete', function(){
     console.log('Delete book' + $(this).data('book'));
     var bookId = $(this).data('book');
@@ -84,22 +91,44 @@ $(function(){
       }
     });//end ajax
   });
-
+//puts the book you want to edit into the inputs
   $('#books').on('click', '.edit', function(){
     console.log('Edit book ' + $(this).data('book') + $(this).data('author') + $(this).data('publisher') + $(this).data('year'));
-    submitFormClear();
+    bookId = $(this).data('book');
+    $('.input').val('');
     $('#title').val($(this).data('title'));
     $('#author').val($(this).data('author'));
     $('#publisher').val($(this).data('publisher'));
     $('#year').val($(this).data('year'));
+    // $('#submit').remove();
+    // $('#submitEdit').remove();
+    // $('#bookForm').last().append('<input type="submit" name="submit" value="submit" id="submitEdit">');
+
+  }); // end of edit function
+
+  $('#submitEdit').on('click', function (event){
+    event.preventDefault();
+    console.log("bookId");
+    $.ajax({
+      type: "PUT",
+      url: "/books/edit",
+      data: {id: bookId, title: $("#title").val(), author:$('#author').val(), publisher: $('#publisher').val(), year: $('#year').val()},
+      success: function (response){
+        getBooks();
+      }
+    });
   });
 
+  function editSubmit (object){
+
+}
   function submitFormClear (){
-    $('#title').val(' ');
-    $('#author').val(' ');
-    $('#publisher').val(' ');
-    $('#year').val(' ');
-    $('#submit').val(' ');
+    $('#title').val('');
+    $('#author').val('');
+    $('#publisher').val('');
+    $('#year').val('');
+    $('#submit').val('');
   }
+
 
 }); // ends document ready function
--- server/routes/books.js
+++ server/routes/books.js
@@ -83,6 +83,33 @@ pool.connect(function(errorConnectingToDatabase, client, done){
 });// end delete function
 
 
+router.put('/edit', function(req, res){
+  console.log(req.body);
+  var title = req.body.title;
+  var author = req.body.author;
+  var publisher = req.body.publisher;
+  var year = req.body.year;
+  var bookId = req.body.id;
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log("Error connecting to DB");
+      res.send(500);
+    } else {
+      console.log("connected");
+      client.query('UPDATE "books" SET "author" = $2, "title" = $3, "publisher" = $4, "year" = $5 WHERE "id" = $1', [bookId, author, title, publisher, year], function(queryError, result){
+        done();
+        if(queryError){
+          console.log("Error making query.");
+          res.send(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});
+
+
 //Get Function to search in database
 router.get('/search', function (req, res){
   console.log("in search path");
