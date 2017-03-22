8804561cc971b35bfe435c4332f331912a669676
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-22 15:11:14 -0500

    edit button and delete button added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -24,10 +24,20 @@ $('#searchForm').on('submit', function (event){
       type: "GET",
       url: "books/search/" + searchTerm + "/",
       success: function(response){
-
+        submitFormClear();
+        $('#/books').empty();
+        var book = response;
+        $("#books").append('<tr>');
+        $el = $('#books').children().last();
+        $el.append('<td>' + book.id + '</td>');
+        $el.append('<td>' + book.author + '</td>');
+        $el.append('<td>' + book.title + '</td>');
+        $el.append('<td>' + book.publisher + '</td>');
+        $el.append('<td>' + book.year + '</td>');
+        $el.append("<td><button>Delete</button></td>");
       }
     });
-});
+});//end click function
 function getBooks(){
   $.ajax({
     type: "GET",
@@ -52,17 +62,42 @@ function getBooks(){
         $el.append('<td>' + book.title + '</td>');
         $el.append('<td>' + book.publisher + '</td>');
         $el.append('<td>' + book.year + '</td>');
-
+        $el.append("<td><button class='delete' data-book='" + book.id + "'>Delete</button></td>");
+        $el.append("<td><button class='edit' data-book='" + book.id + "' data-author='" +
+                        book.author + "' data-title='" + book.title + "' data-publisher='" +
+                        book.publisher + "' data-year='" + book.year + "'>Edit</button></td>");
       } //end for loop
       } //end success function
-    }); // end getBooks
-  }
+    }); // end AJAX
+  } //end getBooks
+
+  $('#books').on('click', '.delete', function(){
+    console.log('Delete book' + $(this).data('book'));
+    var bookId = $(this).data('book');
+    // $.ajax({
+    //   type: "DELETE",
+    //   url: "books/delete/" + bookId + "/",
+    //   success: function (){
+    //     console.log("in delete book path");
+    //     getBooks();
+    //   }
+    });//end ajax
+
+  $('#books').on('click', '.edit', function(){
+    console.log('Edit book ' + $(this).data('book') + $(this).data('author') + $(this).data('publisher') + $(this).data('year'));
+    submitFormClear();
+    $('#title').val($(this).data('title'));
+    $('#author').val($(this).data('author'));
+    $('#publisher').val($(this).data('publisher'));
+    $('#year').val($(this).data('year'));
+    });
 
   function submitFormClear (){
-    $('#title').val('');
-    $('#author').val('');
-    $('#publisher').val('');
-    $('#year').val('');
+    $('#title').val(' ');
+    $('#author').val(' ');
+    $('#publisher').val(' ');
+    $('#year').val(' ');
+    $('#submit').val(' ');
   }
 
 }); // ends document ready function
