00ed7209d687aaf4fb85070612fda3ae229df882
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-22 10:48:17 -0500

    appending to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,14 @@ $(function(){
     url: "/books",
     success: function(response){
       console.log("in response path", response);
+      for (var i = 0; i < response.length; i++) {
+        var book = response[i];
+        $('#books').append('<tr>');
+        var $el = $('#books').children().last();
+        $el.append('<td>' + book.id + '</td>');
+        $el.append('<td>' + book.author + '</td>');
+        $el.append('<td>' + book.title + '</td>');
+      }
     }
   });
 });
